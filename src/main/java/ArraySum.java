9d9e8148ce18c8a002d4b704d0cc72fc3f06f630
THECODEMANZ <kewabailey@gmail.com>
2023-03-01 04:33:38 +0000

    Completed Lab ArraySumTest

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -6,8 +6,14 @@ public class ArraySum {
      * @param arr an array of ints
      * @return the sum of all values in arr.
      */
+
+     public int sum = 0;
     public int sum(int[] arr){
 
-        return 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+
+        return sum;
     }
 }
